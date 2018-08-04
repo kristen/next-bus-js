33202021f197eec4c7254413ef953579293f0c30
Kristen Sundquist <kristen@sigfig.com>
2018-08-04 07:35:18 -0700

    Improve font size of BusTag

--- app/components/BusTag/index.js
+++ app/components/BusTag/index.js
@@ -6,6 +6,12 @@
 
 import styled from 'styled-components';
 
+const fontSize = tag => {
+  if (tag.length > 3) return 14;
+  if (tag.length > 2) return 18;
+  return 20;
+};
+
 const BusTag = styled.div`
   background: ${props => (props.color && `#${props.color}`) || 'lightgrey'};
   color: ${props =>
@@ -15,7 +21,7 @@ const BusTag = styled.div`
   height: ${props => props.size}px;
   text-align: center;
   line-height: ${props => props.size}px;
-  font-size: ${props => (props.children.length > 2 ? 12 : 20)}px;
+  font-size: ${props => fontSize(props.children)}px;
 `;
 
 export default BusTag;
